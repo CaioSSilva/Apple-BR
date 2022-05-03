e22c605e57d9878d4e5d45329f039b08f2f51774
Caio Souza Silva <caiosouzasilva13650@gmail.com>
2022-05-02 22:09:50 -0300

    Url add

--- iPhone 13 Pro buy Page/image-change.js
+++ iPhone 13 Pro buy Page/image-change.js
@@ -1,5 +1,5 @@
 const imagem = document.getElementById("iphone-image");
-const iP13Pro = document.getElementById("iphone-13-pro")
+const iP13Pro = document.getElementById("iphone-13-pro");
 iP13Pro.addEventListener('click', () =>{
-    imagem.src = 
+    imagem.src = "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-pro-family-select?wid=940&amp;hei=1112&amp;fmt=jpeg&amp;qlt=90&amp;.v=1645811022162";
 })
